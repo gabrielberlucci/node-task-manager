6ad06b6c15c0cd4a021810d181f81687422ddb98
gabriel.berlucci <gabrielberlucci@proton.me>
2026-07-01 03:20:55 -0300

    feat: add error middleware

--- app/src/server.ts
+++ app/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { routes } from '@/routes';
 import cors from 'cors';
+import { validateErrors } from '@/middlewares';
 
 const app = express();
 const port = 3000;
@@ -8,6 +9,7 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(validateErrors);
 
 app.listen(port, () => {
   console.log(`Task Manager is running on port ${port}`);
