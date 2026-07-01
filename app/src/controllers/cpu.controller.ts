a1649c3dc4b24a82a2e61e5fac5b17b6a9e020fc
gabriel.berlucci <gabrielberlucci@proton.me>
2026-07-01 03:20:41 -0300

    feat: add controllers

--- app/src/controllers/cpu.controller.ts
+++ app/src/controllers/cpu.controller.ts
@@ -1,7 +1,13 @@
+import { getStaticCpuInfo } from '@/services';
 import type { Request, Response } from 'express';
+import { StatusCodes, ReasonPhrases } from 'http-status-codes';
 
-export const cpuController = async (req: Request, res: Response) => {
-  res.status(200).send({
-    message: 'Hello, world!',
+export const cpuController = async (_req: Request, res: Response) => {
+  const data = await getStaticCpuInfo();
+
+  res.status(StatusCodes.OK).send({
+    status: ReasonPhrases.OK,
+    message: 'Static CPU info fetched with success',
+    data: data,
   });
 };
