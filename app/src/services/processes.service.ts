b5e734b40a53c6159902f9d0d44e054c4e3b3dbf
gabriel.berlucci <gabrielberlucci@proton.me>
2026-07-09 02:27:17 -0300

    feat: remove -First filter

--- app/src/services/processes.service.ts
+++ app/src/services/processes.service.ts
@@ -6,7 +6,7 @@ import child_process from 'node:child_process';
 export const getTopProcesses = async () => {
   const exec = promisify(child_process.exec);
 
-  const command = `powershell.exe -NoProfile -Command "Get-Process | Where-Object {$_.ProcessName -notin ('svchost', 'explorer', 'System', 'Idle', 'Registry', 'smss', 'csrss', 'wininit', 'services', 'lsass', 'fontdrvhost', 'dwm', 'taskhostw', 'ctfmon', 'conhost', 'SearchIndexer', 'ApplicationFrameHost')} |Sort-Object WS -Descending  | Select Id, ProcessName, WS -First 30 | ConvertTo-Json"`;
+  const command = `powershell.exe -NoProfile -Command "Get-Process | Where-Object {$_.ProcessName -notin ('svchost', 'explorer', 'System', 'Idle', 'Registry', 'smss', 'csrss', 'wininit', 'services', 'lsass', 'fontdrvhost', 'dwm', 'taskhostw', 'ctfmon', 'conhost', 'SearchIndexer', 'ApplicationFrameHost')} |Sort-Object WS -Descending  | Select Id, ProcessName, WS | ConvertTo-Json"`;
 
   const { stdout, stderr } = await exec(command);
 
